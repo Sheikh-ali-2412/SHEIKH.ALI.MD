b0273cc6ca88c53123eb375c0a8b3e6c973e43aa
Ali Ahmad Farooq <aliahmad747426@gmail.com>
2024-12-22 20:04:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gLEnXCgY#Z8-_Q-ySAhCfGBrqV-CJ8TotLmje0cOB9-G8NWi1zrY",
+SESSION_ID: process.env.SESSION_ID || "TuwkEbbb#WtsBqZxbELfYGMvxvA3F8Cy4WJE0HNrsMJ3xL3IV2C4",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
@@ -15,7 +15,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/m1rf91.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY SIR/MAM SOBIA MD IS AVAILABLE FOR YOUR HELP",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "$",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
 OWNER_REACT: process.env.OWNER_REACT || "true",
